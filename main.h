7bb59ab0e076afdcf20b2a67ba4ccd81b04ea557
Lornzyy <lornanish@gmail.com>
2022-10-20 13:18:23 -0700

    Message

--- main.h
+++ main.h
@@ -27,7 +27,7 @@
 struct fmt
 {
 	char fmt;
-	int (*fn)(va_llist, char[], int, int, int, int);
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 
@@ -38,7 +38,7 @@ struct fmt
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
-int _print(const char *format, ...);
+int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
